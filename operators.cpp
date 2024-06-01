8e60e8578e45a4a1886fca6e45bcdfb15c51952a
Prithvi Raj <138392587+prithvikings@users.noreply.github.com>
2024-06-01 17:50:35 +0000

    way of taking input

---
+++ operators.cpp
@@ -0,0 +1,34 @@
+//operators
+#include<iostream>
+using namespace std;
+int main(){
+    //Arithmetic Operator +,-,*,/,%
+    int a=10;
+    int b=2;
+    int add=a+b;
+    int sub=a-b;
+    int mul=a*b;
+    int div=a/b;
+    int modu=a%b;
+    cout<<add<<endl;//Addition: 12
+    cout<<sub<<endl;//Subtraction: 8
+    cout<<mul<<endl;//Multiplication: 20
+    cout<<modu<<endl;//Remainder: 5
+    cout<<div<<endl;//Division: 5
+
+    //Relational Operator <,>,=,!=,<=,>=;
+
+    bool first=a==b;
+    bool second=a<b;
+    bool third=a>b;
+    bool fourth=a!=b;
+    cout<<first<<endl;
+    cout<<second<<endl;
+    cout<<third<<endl;
+    cout<<fourth<<endl;
+
+    //assignment operator =;
+
+    //logical operator &&,||,!;
+    
+}
