de7036bdbec40dcc87e17942b10a2abd33b6efdb
Prithvi Raj <138392587+prithvikings@users.noreply.github.com>
2024-06-01 17:48:51 +0000

    operators of c++

---
+++ typecasting.cpp
@@ -0,0 +1,11 @@
+//Typecasting
+#include<iostream>
+using namespace std;
+int main(){
+    char a=97;
+    int b='a';
+    cout <<"Character typecast from int to char"<<endl;
+    cout<<a<<endl;
+    cout <<"Character typecast from char to int"<<endl;
+    cout<<b<<endl;
+}
