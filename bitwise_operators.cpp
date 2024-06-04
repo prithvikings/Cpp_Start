de988971e3e2e1a7e6a7dd11babee09b047c5fa8
Prithvi Raj <138392587+prithvikings@users.noreply.github.com>
2024-06-04 15:56:39 +0530

    Create bitwise_operators.cpp

---
+++ bitwise_operators.cpp
@@ -0,0 +1,25 @@
+//AND, OR, NOT, XOR
+//Bitwise operator padhna hai
+/*#include<iostream>
+using namespace std;
+int main(){
+    int a=4;
+    int b=6;
+    cout<<"a&b"<<(a&b) <<endl;
+    cout<<"a|b"<<(a|b) <<endl;
+    cout<<"~a"<<~a <<endl;
+    cout<<"a^b"<<(a^b) <<endl;
+}*/
+
+//Preincrement,Postincrement,Predecrement,Postdecrement
+/*#include<iostream>
+using namespace std;
+int main(){
+    int i=8;
+    cout<<"i++ : "<<i++<<endl;
+    cout<<"++i : "<<++i<<endl;
+    cout<<"i-- : "<<i--<<endl;
+    cout<<"--i  : "<<--i<<endl;
+    return 0;
+}*/ 
+
