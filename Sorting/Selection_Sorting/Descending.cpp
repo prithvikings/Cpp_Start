949d273a1ce8eca1fef774d134cee9367a49109f
Prithvi Raj <138392587+prithvikings@users.noreply.github.com>
2024-06-25 12:36:25 +0000

    sorting

---
+++ Sorting/Selection_Sorting/Descending.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={23,1,189,23,7};
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<' ';
+    }cout<<endl;
+    for(int i=0;i<5-1;i++){
+        int index=i;
+        for(int j=i+1;j<5;j++){
+            if(arr[index]<arr[j]){
+                index=j;
+            }
+        }
+        swap(arr[index],arr[i]);
+    }
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<' ';
+    }cout<<endl;
+}
