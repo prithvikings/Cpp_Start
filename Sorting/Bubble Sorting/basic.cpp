d56730b2bea9363f3b5f872da28a06067189a599
Prithvi Raj <138392587+prithvikings@users.noreply.github.com>
2024-06-26 13:01:40 +0000

    Bubble Sort

---
+++ Sorting/Bubble Sorting/basic.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={123,1,23,2,90};
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<' ';
+    }cout<<endl;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5-i-1;j++){
+            if(arr[j]<arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<' ';
+    }cout<<endl;
+}
